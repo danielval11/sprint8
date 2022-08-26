107edca56a8699c3eb993375213d4f409fd221ff
Rociogomez23 <rocioayelengomez19@gmail.com>
2022-08-26 02:26:40 -0300

    Esto es un commit

---
+++ homebanking/API/serializers.py
@@ -0,0 +1,145 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from branches.models import Address, Branch
+from cliente.models import Cliente
+from cuentas.models import Cuenta
+from prestamos.models import Prestamo
+from tarjetas.models import Tarjeta
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    street = serializers.CharField(required=False)
+    number = serializers.IntegerField(required=False)
+    city = serializers.CharField(required=False)
+    state = serializers.CharField(required=False)
+    country = serializers.CharField(required=False)
+    zip_code = serializers.CharField(required=False)
+
+    class Meta:
+        model = Address
+        fields = ['street', 'number', 'city', 'state', 'country', 'zip_code']
+
+    def update(self, instance, validated_data):
+        instance.street = validated_data.get('street', instance.street)
+        instance.number = validated_data.get('number', instance.number)
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+        instance.country = validated_data.get('country', instance.country)
+        instance.zip_code = validated_data.get('zip_code', instance.zip_code)
+        instance.save()
+        return instance
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    address = AddressSerializer(read_only=True)
+
+    class Meta:
+        model = Branch
+        fields = ['name', 'address']
+
+
+class TarjetaSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_type_display')
+
+    class Meta:
+        model = Tarjeta
+        fields = ['brand', 'type', 'number', 'cvv', 'valid_from', 'expiration_end']
+
+
+class CuentaSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_type_display')
+
+    class Meta:
+        model = Cuenta
+        fields = ['iban', 'type', 'alias', 'balance']
+
+
+class PrestamoCreateSerializer(serializers.ModelSerializer):
+    customer = serializers.CharField(source='customer.username', write_only=True)
+    date = serializers.DateTimeField(format="%Y-%m-%d", required=False, default=datetime.now)
+
+    class Meta:
+        model = Prestamo
+        fields = ['type', 'date', 'total', "customer", "date"]
+
+    def validate(self, data):
+        amount = data['total']
+
+        try:
+            amount = float(amount)
+            if amount <= 0:
+                raise serializers.ValidationError("Amount must be greater than 0")
+        except ValueError:
+            raise serializers.ValidationError("Amount must be a number")
+
+        loan_type = data['type']
+        if loan_type not in Prestamo.LoanType.values:
+            raise serializers.ValidationError("Invalid loan type")
+
+        username = data['customer']["username"]
+        if username is None:
+            raise serializers.ValidationError("Username is required")
+
+        try:
+            customer = User.objects.get(username=username).cliente
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Customer does not exists")
+
+        max_loan = Prestamo.get_max_loan(customer)
+
+        if amount > max_loan:
+            raise serializers.ValidationError("Amount is greater than the maximum allowed loan")
+
+        return data
+
+    def create(self, validated_data):
+        customer = User.objects.get(username=validated_data['customer']["username"]).cliente
+        loan = Prestamo(
+            customer=customer,
+            type=validated_data['type'],
+            total=validated_data['total'],
+            date=validated_data['date']
+        )
+        loan.save()
+        return loan
+
+
+class PrestamoSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_type_display')
+
+    class Meta:
+        model = Prestamo
+        fields = ['type', 'date', 'total']
+
+
+class ClienteSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_type_display')
+    address = AddressSerializer(read_only=True)
+
+    class Meta:
+        fields = ('type', 'dni', 'address',)
+        model = Cliente
+
+
+class UserSerializer(serializers.ModelSerializer):
+    customer = ClienteSerializer(many=False, read_only=True, source='cliente')
+    accounts = CuentaSerializer(many=True, read_only=True, source='cuenta_set')
+    cards = TarjetaSerializer(many=True, read_only=True, source='tarjeta_set')
+    loans = PrestamoSerializer(many=True, read_only=True, source='cliente.prestamo_set')
+    extra_kwargs = {'password': {'write_only': True}}
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'customer', 'accounts', "cards", 'loans')
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ homebanking/API/views.py
@@ -0,0 +1,218 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from branches.models import Branch, Address
+from prestamos.models import Prestamo
+from tarjetas.models import Tarjeta
+from .serializers import UserSerializer, PrestamoSerializer, BranchSerializer, TarjetaSerializer, \
+    PrestamoCreateSerializer, AddressSerializer
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def customer_data(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def customer_me_data(request):
+    try:
+        user = User.objects.get(pk=request.user.pk)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({'status': 'User does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def branches_data(request):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+
+    branches = Branch.objects.all()
+    serializer = BranchSerializer(branches, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def branch_data(request, pk):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        loans = Prestamo.objects.prefetch_related('customer').filter(customer__branch__pk=pk)
+        serializer = PrestamoSerializer(loans, many=True)
+        return Response(serializer.data)
+    except Branch.DoesNotExist:
+        return Response({'status': 'No branch found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def cards_data(request):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+
+    credit_cards = Tarjeta.objects.filter(type=Tarjeta.CardType.CREDIT)
+    serializer = TarjetaSerializer(credit_cards, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def user_data(request, username):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+    try:
+        user = User.objects.get(username=username)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({'status': 'User does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def cards_customer_data(request, username):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+
+    try:
+        credit_cards = User.objects.get(username=username).tarjeta_set.filter(type=Tarjeta.CardType.CREDIT)
+        serializer = TarjetaSerializer(credit_cards, many=True)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({'status': 'User does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+def add_loan(request):
+    serializer = PrestamoCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_loan(request, pk):
+    if not request.user.is_staff and request.user.employee is None:
+        query = request.user.cliente.prestamo_set
+    else:
+        query = Prestamo.objects
+
+    try:
+        loan = query.get(pk=pk)
+        serializer = PrestamoSerializer(loan, many=False)
+        return Response(serializer.data)
+    except Prestamo.DoesNotExist:
+        return Response({'status': 'No loan found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+def get_loans(request):
+    loans = Prestamo.objects.all()
+    serializer = PrestamoSerializer(loans, many=True)
+    return Response(serializer.data)
+
+
+def delete_loan(request, pk):
+    if not request.user.is_staff and request.user.employee is None:
+        try:
+            loan = request.user.cliente.prestamo_set.get(pk=pk)
+        except Prestamo.DoesNotExist:
+            return Response({'status': 'Requested LoanLoan does not exists'}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        try:
+            loan = Prestamo.objects.get(pk=pk)
+        except Prestamo.DoesNotExist:
+            return Response({'status': 'Loan does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+    loan.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST', ])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated, ])
+def loans_data(request):
+    if not request.user.is_staff and request.user.employee is None:
+        return Response({'status': "You don't have enough permissions."}, status=status.HTTP_403_FORBIDDEN)
+
+    if request.method == 'GET':
+        return get_loans(request)
+    elif request.method == 'POST':
+        return add_loan(request)
+    else:
+        return Response({'status': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['GET', 'DELETE'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def loan_data(request, pk):
+    if request.method == 'GET':
+        return get_loan(request, pk)
+    elif request.method == 'DELETE':
+        return delete_loan(request, pk)
+    else:
+        return Response({'status': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+def update_address(request, pk):
+    if not request.user.is_staff and request.user.employee is None:
+        query = request.user.cliente.address
+    else:
+        query = Address.objects
+
+    try:
+        address = query.get(pk=pk)
+    except Address.DoesNotExist:
+        return Response({'status': 'Address does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+    address_serializer = AddressSerializer(address, data=request.data)
+
+    if address_serializer.is_valid():
+        address_serializer.update(instance=address, validated_data=request.data)
+        return Response(address_serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response(address_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_address(request, pk):
+    if not request.user.is_staff and request.user.employee is None:
+        query = request.user.cliente.address
+    else:
+        query = Address.objects
+
+    try:
+        address = query.get(pk=pk)
+        serializer = AddressSerializer(address, many=False)
+        return Response(serializer.data)
+    except Address.DoesNotExist:
+        return Response({'status': 'Address does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'PUT', 'PATCH'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def address_data(request, pk):
+    if request.method == 'GET':
+        return get_address(request, pk)
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        return update_address(request, pk)
+    else:
+        return Response({'status': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
